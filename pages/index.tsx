60619226b8544f54138253e01eef4bf4fbb86906
Flavie <flaviengbosando10@gmail.com>
2021-06-01 20:48:27 +0200

    decoupe_plan_mag suite

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,17 @@
 import React from "react";
 
-
 const HomePage: React.FC = () => {
   return (
-  <>
-   <div className="section">Hello</div>
-  <div className="image"> 
-   <img src="https://m1.lmcdn.fr/media/18/5d0901067eb45f348d8a8c9f/1798045962/map-png-store-3.png" width="1080px" alt="" />
-   <div className="section">Hello</div>
-   </div>
-  </>
-  )
+    <>
+      <div className="image">
+        <img
+          src="https://m1.lmcdn.fr/media/18/5d0901067eb45f348d8a8c9f/1798045962/map-png-store-3.png"
+          alt=""
+        />
+        <div className="section"></div>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;

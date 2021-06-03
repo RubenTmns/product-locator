16280b7eada55f5182331f1295f23019568361da
Flavie <flaviengbosando10@gmail.com>
2021-06-03 10:03:35 +0200

    test de geolocalisation des products dans une catégorie: Sol

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,25 +8,32 @@ const HomePage: React.FC = () => {
           src="https://m1.lmcdn.fr/media/18/5d0901067eb45f348d8a8c9f/1798045962/map-png-store-3.png"
           alt=""
         />
-        <div className="section1"></div>
-        <div className="section2"></div>
-        <div className="section3"></div>
-        <div className="section4"></div>
-        <div className="section5"></div>
-        <div className="section6"></div>
-        <div className="section7"></div>
-        <div className="section8"></div>
-        <div className="section9"></div>
-        <div className="section10"></div>
-        <div className="section11"></div>
-        <div className="section12"></div>
-        <div className="section13"></div>
-        <div className="section14"></div>
-        <div className="section15"></div>
-        <div className="section16"></div>
-        <div className="section17"></div>
-        <div className="section18"></div>
-        <div className="section19"></div>
+        <div className="sol"></div>
+        <div className="cuisine"></div>
+        <div className="rangement"></div>
+        <div className="menuiserie1"></div>
+        <div className="decoration"></div>
+        <div className="peinture1"></div>
+        <div className="sdb"></div>
+        <div className="chauffage"></div>
+        <div className="menuiserie2"></div>
+        <div className="electricite"></div>
+        <div className="plomberie"></div>
+        <div className="peinture2"></div>
+        <div className="quincaillerie1"></div>
+        <div className="outillage"></div>
+        <div className="eclairage"></div>
+        <div className="quincaillerie2"></div>
+        <div className="jardin1"></div>
+        <div className="jardin2"></div>
+        <div className="pointeur"></div>
+        <div className="carelage"></div>
+        <div className="pvc"></div>
+        <div className="poubelle"></div>
+        <div className="hotte"></div>
+        <div className="boiteDeRangement"></div>
+        <div className="lotDe4Cintres"></div>
+        <div className="voletRoulant"></div>
       </div>
     </>
   );

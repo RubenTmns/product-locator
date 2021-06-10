079ea6253bd9be00f3bfa5da5fabefaab018bcbd
Aboubacar Keita <aboubacarkeita585@gmail.com>
2021-06-10 09:41:30 +0200

    SearchBar

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,8 +19,8 @@ const Acceuil: React.FC<{ search }> = ({ search }) => {
           />
         </div>
 
-        <div className="input-group w-50">
-          <form action="/api/accueil" method="POST">
+        <form action="/api/accueil" method="POST">
+          <div className="input-group " style={{ width: "50rem" }}>
             <input
               type="text"
               name="input"
@@ -46,8 +46,9 @@ const Acceuil: React.FC<{ search }> = ({ search }) => {
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path>
               </svg>
             </button>
-          </form>
-        </div>
+          </div>
+        </form>
+
         <h1 className="mt-5">Chaque recherche vous fait gagner du temps</h1>
       </div>
     </>
